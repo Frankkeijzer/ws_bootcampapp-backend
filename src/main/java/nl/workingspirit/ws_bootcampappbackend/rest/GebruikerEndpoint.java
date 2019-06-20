b446e2f546757733b019fecc45e9012c133276d3
Frankkeijzer <f.keijzer@workingspirit.nl>
2019-06-20 10:29:07 +0200

    Een aantal niet noodzakelijke imports verwijderd

--- src/main/java/nl/workingspirit/ws_bootcampappbackend/rest/GebruikerEndpoint.java
+++ src/main/java/nl/workingspirit/ws_bootcampappbackend/rest/GebruikerEndpoint.java
@@ -1,8 +1,6 @@
 package nl.workingspirit.ws_bootcampappbackend.rest;
 
-import java.util.List;
 import java.util.Optional;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
--- src/main/java/nl/workingspirit/ws_bootcampappbackend/rest/InlogEndphoint.java
+++ src/main/java/nl/workingspirit/ws_bootcampappbackend/rest/InlogEndphoint.java
@@ -3,9 +3,7 @@ package nl.workingspirit.ws_bootcampappbackend.rest;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
