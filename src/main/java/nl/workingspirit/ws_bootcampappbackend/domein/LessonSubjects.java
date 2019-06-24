5d3e0757479a25ec24d580ef7fb880d73985d24b
Frankkeijzer <f.keijzer@workingspirit.nl>
2019-06-24 22:14:11 +0200

    Files in Domein opgeschoond van lege regels en de kolommen description in Assignment en code in LessonsSubjects vergroot naar 10000

--- src/main/java/nl/workingspirit/ws_bootcampappbackend/domein/LessonSubjects.java
+++ src/main/java/nl/workingspirit/ws_bootcampappbackend/domein/LessonSubjects.java
@@ -8,41 +8,40 @@ import javax.persistence.Id;
 
 @Entity
 public class LessonSubjects {
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Long id;
-private String day;
-private String title;
-@Column(length = 1000)
-private String code;
-@Column(length = 10000)
-private String description;
-public Long getId() {
-	return id;
-}
-public String getDay() {
-	return day;
-}
-public void setDay(String day) {
-	this.day = day;
-}
-public String getTitle() {
-	return title;
-}
-public void setTitle(String title) {
-	this.title = title;
-}
-public String getCode() {
-	return code;
-}
-public void setCode(String code) {
-	this.code = code;
-}
-public String getDescription() {
-	return description;
-}
-public void setBeschrijving(String description) {
-	this.description = description;
-}
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String day;
+	private String title;
+	@Column(length = 10000)
+	private String code;
+	@Column(length = 10000)
+	private String description;
+	public Long getId() {
+		return id;
+	}
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setBeschrijving(String description) {
+		this.description = description;
+	}
 }
