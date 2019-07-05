555d2da021da229268761b0a33aa7318d35a51aa
KikkertJ <j.kikkert@workingspirit.nl>
2019-07-05 14:45:32 +0200

    assignmentTest opmaak

--- src/test/java/nl/workingspirit/ws_bootcampappbackend/controller/AssignmentDeleteServiceTest.java
+++ src/test/java/nl/workingspirit/ws_bootcampappbackend/controller/AssignmentDeleteServiceTest.java
@@ -1,24 +1,17 @@
 package nl.workingspirit.ws_bootcampappbackend.controller;
 
-import org.assertj.core.util.Arrays;
 import org.junit.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.stubbing.Answer;
 
 import nl.workingspirit.ws_bootcampappbackend.domein.Assignment;
-import nl.workingspirit.ws_bootcampappbackend.domein.Role;
-import nl.workingspirit.ws_bootcampappbackend.domein.User;
 
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 import static org.mockito.Mockito.when;
-import static org.mockito.ArgumentMatchers.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -28,17 +21,6 @@ public class AssignmentDeleteServiceTest {
 
     @InjectMocks
     private AssignmentDeleteService sut;
-
-    @Before
-    public void setUp() {
- 
-    	
-    }
-
-    @After
-    public void tearDown() {
-
-    }
     
     @Test
     public void deleteAssignmentSucceeds() {	
@@ -49,8 +31,8 @@ public class AssignmentDeleteServiceTest {
     	//Do
     	when(assignmentRepository.findById(id)).thenReturn(Optional.of(assignment));
     	boolean succeeded = sut.deleteAssignment(id);
-    	
-    	
+    	  
+    	//Assert
     	Assert.assertTrue(succeeded);
     	Mockito.verify(assignmentRepository).findById(Mockito.eq(id));
     	
