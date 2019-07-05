2476d7d7ada9695080120faad4e4b71a191b86bd
KikkertJ <j.kikkert@workingspirit.nl>
2019-07-05 10:26:09 +0200

    refactor

--- src/main/java/nl/workingspirit/ws_bootcampappbackend/controller/LessonSubjectsUpdateService.java
+++ src/main/java/nl/workingspirit/ws_bootcampappbackend/controller/LessonSubjectsUpdateService.java
@@ -7,7 +7,6 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import nl.workingspirit.ws_bootcampappbackend.domein.Assignment;
 import nl.workingspirit.ws_bootcampappbackend.domein.LessonSubjects;
 
 @Service
--- src/main/java/nl/workingspirit/ws_bootcampappbackend/rest/AssignmentEndpoint.java
+++ src/main/java/nl/workingspirit/ws_bootcampappbackend/rest/AssignmentEndpoint.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,7 +64,7 @@ public class AssignmentEndpoint {
 		return ResponseEntity.ok(assignmentRequestService.requestAssignmentByID(id));
 	}
 	
-	@PostMapping("DeleteAssignment/{id}")
+	@DeleteMapping("DeleteAssignment/{id}")
 	public void deleteAssignment (@PathVariable Long id) {
 		assignmentDeleteService.deleteAssignment(id);
 	}
